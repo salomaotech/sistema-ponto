12dbebf943785b6ed82eb2752f18af1263d74976
salomaotech <salomao@taimber.com>
2025-05-09 15:23:21 -0300

    Ordenando funcionarios por nome

--- src/main/java/com/ana/coutinho/ponto/repository/FuncionariosRepository.java
+++ src/main/java/com/ana/coutinho/ponto/repository/FuncionariosRepository.java
@@ -1,14 +1,22 @@
 package com.ana.coutinho.ponto.repository;
 
 import com.ana.coutinho.ponto.model.Funcionarios;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FuncionariosRepository extends JpaRepository<Funcionarios, Long> {
 
-    // Método (LIKE)
+    // Sobrescrevendo findByCpfContaining para ordenar por nome em ordem crescente
+    @Query("SELECT f FROM Funcionarios f WHERE f.cpf LIKE %:cpf% ORDER BY f.nome ASC")
     List<Funcionarios> findByCpfContaining(String cpf);
+
+    // Sobrescrevendo findAll para ordenar por nome em ordem crescente
+    @Override
+    @Query("SELECT f FROM Funcionarios f ORDER BY f.nome ASC")
+    List<Funcionarios> findAll();
 
 }
